fa3a37ad72690958e181c12eba465a9891e671ff
WingeDD <dk0stenko@yandex.ru>
2019-03-10 23:36:06 +0300

    +

--- server/server_test.go
+++ server/server_test.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"reflect"
 	"testing"
+	"time"
 )
+
+type testCase struct {
+	requests1  []string
+	requests2  []string
+	responses1 []string
+	responses2 []string
+	order      []byte //1121111221111121...
+}
 
 func TestGetConfig(t *testing.T) {
 	rooms := []string{"room1", "room2", "room3", "room4"}
@@ -30,4 +41,27 @@ func TestGetConfig(t *testing.T) {
 	} else if err == nil {
 		t.Errorf("Error is nil while using bad config file")
 	}
+}
+
+func TestMain(t *testing.T) {
+	go main()
+	time.Sleep(time.Second)
+	cfg, _ := getConfig(configFileName)
+	conn1, err := net.Dial("tcp", cfg.Address)
+	if err != nil {
+		t.Error("Can`t connect to server")
+	}
+	// conn2, err := net.Dial("tcp", cfg.Address)
+	// if err != nil {
+	// 	t.Error("Can`t connect to server")
+	// }
+	t.Log("TEST 2.1 passed")
+	resp := make([]byte, 1024)
+	conn1.Read(resp)
+	fmt.Println(string(resp))
+	conn1.Write([]byte("qwweqwe"))
+	time.Sleep(time.Millisecond)
+	conn1.Read(resp)
+	fmt.Println(string(resp))
+	fmt.Println("end")
 }
